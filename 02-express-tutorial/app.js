53e1acdccfa34cba3a5267f58f54108bd4724e37
Esa Torkkila <esa.torkkila@gmail.com>
2026-02-02 16:55:03 +0200

    10-middleware-options

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,24 +1,27 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+const logger = require('./logger') 
+const authorize = require('./authorize')
 
-// req => middleware => res
+// setup static and middleware
+// app.use(express.static('./public'))
 
-const logger = (req,res,next) =>{
-    const method = req.method
-    const url = req.url
-    const time = new Date().getFullYear()
-    console.log(method,url,time)
-next()
-}
-app.get('/', logger, (req,res)=>{
+app.use(morgan('tiny'))
 
-  res.send('home')
+
+app.get('/', (req, res) => {
+  res.send('Home')
 })
-
-app.get('/about', logger, (req,res)=>{
-  
-
-  res.send('about')
+app.get('/about', (req, res) => {
+  res.send('About')
+})
+app.get('/api/products', (req, res) => {
+  res.send('Products')
+})
+app.get('/api/items', (req, res) => {
+  console.log(req.user)
+  res.send('Items')
 })
 
 app.listen(5000, () => {
