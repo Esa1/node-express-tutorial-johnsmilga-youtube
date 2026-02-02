9a00a23199c5f5aa0c0b73ec98c0e5ffb7eacb70
Esa Torkkila <esa.torkkila@gmail.com>
2026-02-02 15:07:33 +0200

    Express app developing

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,20 +1,57 @@
 const express = require('express')
 const app = express()
+const {products} = require('./data')
+
+app.use(express.static('./public'))
 
 app.get('/', (req, res) => {
-    res.status(404).send('Home page')
+ 
+    res.send('<h1>Home page</h1><a href="api/products">products</a>')
 })
 
-app.get('/about', (req, res) => {
-    res.status(404).send('About page')
+app.get('/api/products', (req, res)=>{
+    const newProducts = products.map((product)=>{
+        const {id,name,image} = product
+        return {id,name,image}
+    })
+    res.json(newProducts)
 })
 
-app.all('*', (req, res)=>{
-    res.status(404).send('<h1>resource not found</h1>')
+app.get('/api/products/:productId', (req,res) => {
+    const {productId} = req.params
+    const singleProduct = products.find((product)=> product.id === Number(productId))
+    if (!singleProduct){
+        return res.status(404).send('Product not found')
+    }
+    res.json(singleProduct)
+
 })
-app.listen(5000, () => {
-    console.log("listening port 5000")
+
+app.get('/api/products/:productID/reviews/:reviewID', (req, res)=>{
+    console.log(req.params)
+    res.send('hello world')
 })
 
+app.get('/api/v1/query', (req,res)=>{
+    // console.log(req.query)
+    const {search,limit} = req.query
+    let sortedProducts = [...products]
+    if (search){
+        sortedProducts = sortedProducts.filter((product)=>{
+            return product.name.startsWith(search)
+        })
+    }
+    if (limit){
+        sortedProducts = sortedProducts.slice(0,Number(limit))
+    }
+    if (sortedProducts.length < 1){
+        // res.status(200).send('no product found')
+        res.status(200).json({ success: true,  data: []})
 
+    }
 
+    res.status(200).json(sortedProducts)
+})
+app.listen(5000, ()=>{
+    console.log('server listening port 5000...')
+})
